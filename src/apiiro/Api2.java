d367c15355e668b89c7cd64975d5d3e2846a2cee
ohadApiiro <ohad@apiiro.com>
2021-06-06 18:24:39 +0300

    bla

--- src/apiiro/Api2.java
+++ src/apiiro/Api2.java
@@ -2,6 +2,9 @@ package apiiro;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.*;
 import org.apache.commons.crypto.cipher.*;
 
